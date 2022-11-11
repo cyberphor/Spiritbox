d2075f30447da58c5c3343d637f6d36f4dbee817
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-11-10 21:38:53 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,7 @@
+import java.awt.*;
+import java.awt.event.*;
 import java.time.LocalDateTime;
+import javax.swing.*;
 
 class ThreatActor {
     String name;
@@ -31,9 +34,72 @@ class Indicator {
     String[] techniques; // MITRE ATT&CK
 }
 
+class IndicatorForm extends JFrame { 
+    public IndicatorForm() {
+        setTitle("Add Indicator");
+        setBounds(300, 90, 900, 600);
+        JPanel windowPane = new JPanel();
+        this.add(windowPane);
+
+        JLabel reportNumberField = new JLabel("Report Number");
+        reportNumberField.setLocation(50, 50);
+        windowPane.add(reportNumberField);
+
+        JTextField reportNumberValue = new JTextField();
+        reportNumberValue.setColumns(20);
+        reportNumberValue.setLocation(100, 100);
+        windowPane.add(reportNumberValue);
+
+        /*
+        JLabel locationField = new JLabel("Location");
+        windowPane.add(locationField);
+
+        JTextField locationValue = new JTextField();
+        windowPane.add(locationValue);
+
+        dateTimeGroup = new JLabel("Date Time Group");
+        windowPane.add(dateTimeGroup);
+        
+        tactic = new JLabel("Tactic");
+        windowPane.add(tactic);
+
+        sourceIpAddress = new JLabel("Source IP Address");
+        windowPane.add(sourceIpAddress);
+
+        destinationIpAddress = new JLabel("Destination IP Address");
+        windowPane.add(destinationIpAddress);
+
+        destinationHostname = new JLabel("Destination Hostname");
+        windowPane.add(destinationHostname);
+
+        direction = new JLabel("Direction");
+        windowPane.add(direction);
+
+        numberOfSystemsAffected = new JLabel("Number of Systems Affected");
+        windowPane.add(numberOfSystemsAffected);
+
+        actionsTaken = new JLabel("Actions Taken");
+        windowPane.add(actionsTaken);
+        
+        identificationMethod = new JLabel("Identification Method");        
+        windowPane.add(identificationMethod);
+        */
+
+        JButton addButton = new JButton("Add");
+        addButton.addActionListener(e -> 
+            System.out.println("Mouse clicked")
+        );
+        // windowPane.add(addButton);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        //    
+        IndicatorForm indicatorForm = new IndicatorForm();
     }
 }
 
@@ -51,4 +117,7 @@ public class Main {
         numberofSystemsAffected: 1
         actionsTaken: Blocked IP address at perimeter firewall
         identificationMethod: Palo Alto firewall
+        
+    REFERENCES
+        https://www.geeksforgeeks.org/java-swing-simple-user-registration-form/
 */
