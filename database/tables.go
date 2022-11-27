3bd58eaf067f53551e98d0258a73b4b69c4566aa
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-11-27 05:49:34 -0500

    Added another package

---
+++ database/tables.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func AddTable() {
+	database, err := sql.Open("postgres", "spiritbox.db")
+	if err != nil {
+		panic(err)
+	}
+	statement := `CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY,
+		username  TEXT,
+		password  TEXT,
+		team      TEXT,
+		role      TEXT,
+		UNIQUE(username)
+		);`
+	query, err := database.Prepare(statement)
+	if err != nil {
+		panic(err)
+	}
+	query.Exec()
+	database.Close()
+}
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/cyberphor/spiritbox/database"
 	"github.com/cyberphor/spiritbox/web"
 )
 
 func main() {
+	database.AddTable()
 	web.Server()
 }
