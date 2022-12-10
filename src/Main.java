433dbdff9d337dc4d38d0a561bdfdaab205c0cb3
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-12-09 20:47:18 -0500

    Merged tabbed pane infrastructure

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.time.LocalDateTime;
 
-/*
 public class Main {
     public static void main(String[] args) {
         // declare report
@@ -24,46 +23,8 @@ public class Main {
         // get report
         //report.getReport();
 
-        // show menu
-        Menu menu = new Menu();
-        menu.initialize();
+        // display window
+        Window window = new Window();
+        window.display();
     }
 }
-*/
-
-import javax.swing.*;  
-public class Main { 
-    public static void main(String[] args) {  
-        JFrame f = new JFrame();
-        JTabbedPane tp = new JTabbedPane(); 
-        JPanel p1 = new JPanel();  
-        JPanel p2 = new JPanel();  
-        JPanel p3 = new JPanel(); 
-        JPanel p4 = new JPanel(); 
-
-        // add stuff to pane 1
-        // p1.add();
-
-        // add stuff to pane 2
-        // p2.add();
-
-        // add stuff to pane 3
-        // p3.add();
-
-        // add stuff to pane 4
-        // p4.add();
-
-        // add the tabs to the pane
-        tp.setBounds(50,50,200,200);  
-        tp.add("Add", p1);  
-        tp.add("Search", p2);  
-        tp.add("Edit", p3);  
-        tp.add("Monitor", p4); 
-
-        // add the pane to the frame
-        f.add(tp); 
-        f.setSize(400,400);  
-        f.setLayout(null);  
-        f.setVisible(true);
-    }
-} 
