0f2bb95173b7bd6b53efa6226ec0d7e586ff029c
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-02-26 09:21:16 -0800

    Update Spiritbox.psm1

--- Spiritbox.psm1
+++ Spiritbox.psm1
@@ -33,6 +33,7 @@ function Show-SpiritboxError {
     Param(
         [string]$Message
     )
+    # TODO: add Spiritbox icon and make "Error" the label inside the dialog box
     $Button = [System.Windows.Forms.MessageBoxButtons]::OK
     $Icon = [System.Windows.Forms.MessageBoxIcon]::None
     [System.Windows.Forms.MessageBox]::Show($Message, "Error", $Button, $Icon) | Out-Null
